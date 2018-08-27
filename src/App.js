3a283fe3cab76ba0503e362ddaa8cf257b3febde
Michael Solone <michaelsolone@gmail.com>
2018-08-27 16:30:30 -0400

    Can update one letter, but not more

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import words from "./Data/snowman-word-list.json";
-import snowman1 from "./Images/snowman/step_0.png";
+import snowman0 from "./Images/snowman/step_0.png";
+import snowman1 from "./Images/snowman/step_1.png";
+import snowman2 from "./Images/snowman/step_2.png";
+import snowman3 from "./Images/snowman/step_3.png";
+import snowman4 from "./Images/snowman/step_4.png";
+import snowman5 from "./Images/snowman/step_5.png";
+import snowman6 from "./Images/snowman/step_6.png";
+import snowman7 from "./Images/snowman/step_7.png";
 import Button from "./Components/Button";
 
 class App extends Component {
@@ -9,6 +16,7 @@ class App extends Component {
     super(props);
     this.state = {
       secret: words[Math.floor(Math.random() * 1024)],
+      blanks: ['_','_','_','_','_','_','_'],
       alphabet: [
         "A",
         "B",
@@ -48,12 +56,18 @@ class App extends Component {
   addChosenLetter = (letter) => {
     const newPicked = this.state.picked.slice();
     newPicked.push(letter);
+    const newBlanks = this.state.secret.split('').map((l, i) => {
+      if (l.toUpperCase() === letter.toUpperCase()) {
+        return l.toUpperCase() 
+      } else {
+        return "_"
+      }
+    })
+    console.log(newBlanks, letter, newPicked)
     this.setState({
-      picked: newPicked
+      picked: newPicked,
+      blanks: newBlanks
     });
-    
-  
-    
   };
 
   render() {
@@ -61,11 +75,13 @@ class App extends Component {
     return (
       <div className="App">
         <section className="snowman-images">
-          <img src={snowman1} alt="snowman-steps" />
+          <img src={snowman0} alt="snowman-steps" />
         </section>
 
         <section className="blank-letters">
-          <p> _ _ _ _ _ _ _ </p>
+          {this.state.blanks.map((blank, i) => {
+            return <span className='hidden-letters' key={i}>{blank}</span>
+          })}
         </section>
         <section className="alphabet-bank">
           {this.state.alphabet.map((letter, i) => {
--- src/Components/Button.js
+++ src/Components/Button.js
@@ -4,7 +4,7 @@ class Button extends Component {
     render() {
 
         if (this.props.pickedLetters.includes(this.props.letter)) {
-            return <button disabled>*</button>
+            return <button disabled>{this.props.letter}</button>
         } else {
         return (
             <button onClick={() => this.props.addingPickedLetters(this.props.letter)}>
